0e041300c29687d299192c282a9955b7e5869f3c
Aleksid <aleksidys@gmail.com>
2024-08-10 15:52:12 +0500

    10.08.2024

--- src/App.js
+++ src/App.js
@@ -14,20 +14,28 @@ const item = {
 class App extends React.Component {
   render() {
     return (
-      <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="description">
-          {item.descriptionFull}
-        </div>
-        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-        <div className="divider"></div>
-        <div className="purchase-info">
-          <div className="price">{item.currency}{item.price}.00</div>
-          <button>Добавить в корзину</button>
-        </div>
-      </div>
+      <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+    <div className="main-content">
+    <h2>{item.brand}</h2>
+    <h1>{item.title}</h1>
+    <h3>{item.description}</h3>
+    <div className="description">
+    {item.descriptionFull}
+    </div>
+    <div className="divider"></div>
+    <div className="purchase-info">
+      <div className="price">{item.currency}{item.price}.00</div>
+      <button>Добавить в корзину</button>
+    </div>
+  </div>
+    </div>
+  </div>
     );
   }
 }
